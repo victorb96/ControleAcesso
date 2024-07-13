a9f39cfe0c31235ddf5bc6909705e118da6d03a8
Victor Bispo <victorbispo96@outlook.com>
2024-07-13 20:55:48 -0300

    Ajuste docker

--- Infra/Data/Repositories/Base.cs
+++ Infra/Data/Repositories/Base.cs
@@ -3,6 +3,7 @@ namespace GF.ControleAcesso.Infra.Data.Repositories;
 public class Base
 {
     public string GetConnectionString()
-        => "Server=localhost;Database=controle_acesso;Port=8080;User Id=postgres;Password=^Z&yJLm=sbXnmQ[xhMtmPC&eNq7@Xou#;";
+        => //"Server=localhost;Database=controle_acesso;Port=5432;User Id=postgres;Password=^Z&yJLm=sbXnmQ[xhMtmPC&eNq7@Xou#;";
+        $"Server={Environment.GetEnvironmentVariable("DB_HOST")};Database={Environment.GetEnvironmentVariable("DB_NAME")};Port={Environment.GetEnvironmentVariable("DB_PORT")};User Id={Environment.GetEnvironmentVariable("DB_USER")};Password={Environment.GetEnvironmentVariable("DB_PWD")};";
         //Environment.GetEnvironmentVariable("CONNECTION_STR_DB_CONTROLE_ACESSO");
 }
